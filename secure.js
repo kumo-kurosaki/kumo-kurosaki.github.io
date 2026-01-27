f207d9e735b06a09161ee0ffda68a5e582af709e
黒崎くも <kumokurosaki241130@gmail.com>
2026-01-27 14:05:10 +0800

    Update secure.js

--- secure.js
+++ secure.js
@@ -11,7 +11,7 @@ function _v(input) {
 const _d = [
   {
     t: "2025萬聖節",
-    i: "aHR0cHM6Ly9pbWFnZXMucGx1cmsuY29tLzY5WnBsVE1GcmdPb0pBOXFEdzVMUGwucG5n",
+    i: "aHR0cHM6Ly9pbWFnZXMucGx1cmsuY29tLzJFTUtaSzllMlNsN2REMTA1WTRYamwucG5n",
     l: "aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vc2NsL2ZpLzNnM3RqNGF2ZzEzNjk2dXhkdmFlbi8yMDI1LUhhbGxvd2Vlbi1LSy5wbmc/cmwrZXk9NXIyZnpkZnVjdHNwOTAzbHpkYnZhYzZoeCZlPTEmc3Q9emVtYWV5MXcmZGw9MA=="
   },
   {
@@ -21,7 +21,7 @@ const _d = [
   },
   {
     t: "黑崎雪莉",
-    i: "aHR0cHM6Ly9pbWFnZXMucGx1cmsuY29tLzJtZHJRMGM0bjlwUFA0SHlLdjhRaHAucG5n",
+    i: "aHR0cHM6Ly9pbWFnZXMucGx1cmsuY29tLzJFTUtaSzllMlNsN2REMTA1WTRYamwucG5n",
     l: "aHR0cHM6Ly9kcml2ZS5nb29nbGUuY29tL2ZpbGUvZC8xcEV0OV9ZV244YlBwWEhRTF9qa3A3ZFR0SjVRMTF4RVcvdmlldz91c3A9ZHJpdmVfbGluaw=="
   }
 ];
@@ -38,5 +38,6 @@ function getSecureContent(input) {
     link: _b(x.l)
   }));
 }
+
 
 
